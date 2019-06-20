236b14f9c73970f11d7db530ffa225d50b4ebd6c
shuo.ma <shuo.ma@17zuoye.com>
2019-06-20 19:05:04 +0800

    add pin

--- app/src/main/java/com/mgg/demo/mggwidgets/SvgActivity.kt
+++ app/src/main/java/com/mgg/demo/mggwidgets/SvgActivity.kt
@@ -1,15 +1,34 @@
 package com.mgg.demo.mggwidgets
 
 import android.graphics.drawable.Animatable
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_svg.*
 
 class SvgActivity : AppCompatActivity() {
+    var pinAnimEnd=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_svg)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            var animate = iv_anim_pin.drawable as AnimatedVectorDrawable
+            animate.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable?) {
+                    super.onAnimationEnd(drawable)
+                    if (!pinAnimEnd){
+                        animate.start()
+                    }
+                }
+            })
+        }
     }
 
     fun onAndroidClick(view: View) {
@@ -35,6 +54,17 @@ class SvgActivity : AppCompatActivity() {
         if (animate.isRunning) {
             animate.stop()
         } else {
+            animate.start()
+        }
+    }
+
+    fun onPinClick(view: View) {
+        var animate = iv_anim_pin.drawable as Animatable
+        if (animate.isRunning) {
+            pinAnimEnd=true
+            animate.stop()
+        } else {
+            pinAnimEnd=false
             animate.start()
         }
     }
