eafa7d325354bb1e88a49ead4d4bf11755832908
shuo.ma <shuo.ma@17zuoye.com>
2020-07-20 16:02:09 +0800

    支持打包动态修改版本号

--- app/src/main/java/com/mgg/demo/mggwidgets/MainActivity.java
+++ app/src/main/java/com/mgg/demo/mggwidgets/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mgg.demo.mggwidgets;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,15 +17,15 @@ public class MainActivity extends BaseActivity {
     }
 
     public void goRecordButtonActivity(View view) {
-        startActivity(new Intent(MainActivity.this,RecordButtonActivity.class));
+        startActivity(new Intent(MainActivity.this, RecordButtonActivity.class));
     }
 
     public void goRoundImageViewActivity(View view) {
-        startActivity(new Intent(MainActivity.this,RoundImageViewActivity.class));
+        startActivity(new Intent(MainActivity.this, RoundImageViewActivity.class));
     }
 
     public void goSvgActivity(View view) {
-        startActivity(new Intent(MainActivity.this,SvgActivity.class));
+        startActivity(new Intent(MainActivity.this, SvgActivity.class));
     }
 
     public void goCalendarActivity(View view) {
@@ -33,6 +37,6 @@ public class MainActivity extends BaseActivity {
     }
 
     public void goRecordVideoActivity(View view) {
-        startActivity(new Intent(MainActivity.this,RecordVideoActivity.class));
+        startActivity(new Intent(MainActivity.this, RecordVideoActivity.class));
     }
 }
